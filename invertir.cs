3cbbfd46087b482af897ca10eb12c2208495c597
ana-jo-nava01 <anajonava14@gmail.com>
2026-05-24 18:12:57 -0300

    ejercicio 1

---
+++ invertir.cs
@@ -0,0 +1,28 @@
+﻿string? entrada;
+int num = 0;
+
+Console.WriteLine("Ingrese el numero a invertir: ");
+entrada = Console.ReadLine();
+
+if(!int.TryParse(entrada, out num))
+{
+    Console.WriteLine("La entrada ingresada no es un numero.");
+} else
+{
+    if(num <= 0)
+    {
+        Console.WriteLine("El numero es menor o igual que 0");
+    } else
+    {
+        int invertido = 0;
+        while(num > 0)
+        {
+            int dig = num % 10;
+            invertido = (invertido * 10) + dig;
+            num = num / 10;
+        }
+        Console.WriteLine($"Numero invertido: {invertido}");
+    }
+}
+
+
